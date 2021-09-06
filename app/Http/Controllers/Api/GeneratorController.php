8e3b7b0366f1ca93e65385d6434c3fd20a719344
Krasimira <krassi77bg@gmail.com>
2021-09-06 22:56:51 +0300

    Created empty GeneratorController

---
+++ app/Http/Controllers/Api/GeneratorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class GeneratorController extends Controller
+{
+    public function index()
+    {
+        // To do
+    }
+}
