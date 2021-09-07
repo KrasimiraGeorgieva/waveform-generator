5eda9c0d7b69d515c93f9eaa3b8ade894a3881a5
Krasimira <krassi77bg@gmail.com>
2021-09-07 16:43:39 +0300

    Added longest_user_monologue and longest_customer_monologue to return json result

--- app/Http/Controllers/Api/GeneratorController.php
+++ app/Http/Controllers/Api/GeneratorController.php
@@ -25,11 +25,13 @@ class GeneratorController extends Controller
             if (Storage::disk('local')->exists('customer-channel.txt') && $file === 'customer-channel.txt') {
                 $path = Storage::path('customer-channel.txt');
                 $customer = $this->readFile($path);
-                $result['customer'] = $customer;
+                $result['customer'] = $customer['audio_point'];
+                $result['longest_customer_monologue'] = $customer['longest_speech'];
             } elseif (Storage::disk('local')->exists('user-channel.txt') && $file === 'user-channel.txt') {
                 $path = Storage::path('user-channel.txt');
                 $user = $this->readFile($path);
-                $result['user'] = $user;
+                $result['user'] = $user['audio_point'];
+                $result['longest_user_monologue'] = $user['longest_speech'];
             }
         }
 
@@ -47,16 +49,27 @@ class GeneratorController extends Controller
         $audioArr = [];
         $end = 0;
         $i = 0;
+        $max = PHP_INT_MIN;
 
         foreach($contentsArr as $value) {
             if (preg_match('/silence_start: (\d+(\.|)\d+)/', $value, $startMatches )) {
                 $audioArr[$i] = [$end,(double)$startMatches[1]];
+                $speech = (double)$startMatches[1] - $end;
+                if ($speech > $max) {
+                    $max = $speech;
+                }
                 $i++;
+
             } elseif (preg_match('/silence_end: (\d+(\.|)\d+)/', $value, $endMatches )) {
                 $end = (double)$endMatches[1];
+
             }
+
         }
 
-        return $audioArr;
+        return [
+            'audio_point' => $audioArr,
+            'longest_speech' => number_format($max, 2)
+        ];
     }
 }
